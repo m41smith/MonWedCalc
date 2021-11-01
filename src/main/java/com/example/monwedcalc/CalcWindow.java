0ae43b0fd2267a7bb9d5ce60b01349ad863c79ec
Marcus <m41smith@student.bridgew.edu>
2021-11-01 15:06:23 -0400

    started button stuff

--- src/main/java/com/example/monwedcalc/CalcWindow.java
+++ src/main/java/com/example/monwedcalc/CalcWindow.java
@@ -1,4 +1,19 @@
 package com.example.monwedcalc;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
 public class CalcWindow {
+    @FXML
+    private TextField numberField;
+
+    @FXML
+    public void numberButtonPressed(ActionEvent event){
+        var pressedButton = (Button)event.getSource();
+        var buttonText = pressedButton.getText();
+        var currentNumberText = numberField.getText();
+        numberField.setText(currentNumberText+buttonText);
+    }
 }
